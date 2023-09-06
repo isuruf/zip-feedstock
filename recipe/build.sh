4908eef93c4f9fc213adcf95477fee40aa4f7309
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-06 07:46:15 -0500

    remove bzip2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,19 @@
 
 set -eux
 
-# apply patches from fedora
+export CFLAGS="${CFLAGS} -DLARGE_FILE_SUPPORT -DZIP64_SUPPORT" \
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p bzip2
+
+# patch in default conda-forge compiler flags
+sed -i unix/Makefile -e "s|^CFLAGS_NOOPT =|CFLAGS_NOOPT = $CFLAGS $CPPFLAGS |"
+sed -i unix/configure -e "s|^LFLAGS1=''|LFLAGS1='$LDFLAGS'|"
+
+# apply security patches from fedora
 patch -p1 -i zip-3.0-currdir.patch
 patch -p1 -i zip-3.0-exec-shield.patch
 patch -p1 -i zip-3.0-format-security.patch
 patch -p1 -i zipnote.patch
 
-make -f unix/Makefile \
-  generic \
-  prefix="${PREFIX}" \
-  CC="${CC}" \
-  CPP="${CXX}" \
-  CFLAGS="${CFLAGS} -DLARGE_FILE_SUPPORT -DZIP64_SUPPORT -DBZIP2_SUPPORT" \
-  install
+make -f unix/Makefile generic prefix="${PREFIX}" CC="${CC}" CPP="${CC_FOR_BUILD} -E" install
